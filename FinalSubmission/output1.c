edfd0dffb57cd3bd374d81da1b08ff07b53a3766
ImmortalGazelle <36750818+ImmortalGazelle@users.noreply.github.com>
2023-10-25 16:37:00 -0500

    Update output1.c

--- FinalSubmission/output1.c
+++ FinalSubmission/output1.c
@@ -1,4 +1,7 @@
-//Author: Chris Settle
+// Group Number: Group H
+// Author: Chris Settle
+// Email: chris.settle@okstate.edu
+// Date: 10/25/2023
 //Displays a gantt chart of the processes in the SJF algorithim method.
 
 #include <stdio.h>
